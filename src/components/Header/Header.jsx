a0975f85df36af1225cc3dd3404579606f13ae5f
CaroDiego <dicafre1404@gmail.com>
2025-02-18 12:25:43 +0100

    add logged dropdownButton

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import Navbar from "./Navbar";
 import "./Header.css";
 import { IoPerson } from "react-icons/io5";
-import SignUp from "../Login/SignUp";
-import LogIn from "../Login/Login";
+import SignUp from "../User/SignUp";
+import LogIn from "../User/Login";
 import { useContext, useState } from "react";
 import { UserContext } from "../../context/usercontext";
+import { Dropdown, IconButton } from "rsuite";
 
 function Header() {
   const { user, isUserloggedIn } = useContext(UserContext);
@@ -13,39 +14,58 @@ function Header() {
 
   function toggle() {
     setSeen(!seen);
-    if (!seen) {
-      document.body.classList.add("popup-active");
-    } else {
-      document.body.classList.remove("popup-active");
-    }
+    document.body.classList.toggle("popup-active", !seen);
   }
 
   function toggleForm() {
     setIsSignUp(!isSignUp);
   }
 
+  const renderIconButton = (props, ref) => {
+    return (
+      <IconButton
+        {...props}
+        ref={ref}
+        icon={<IoPerson />}
+        className="login-icon"
+      />
+    );
+  };
+
+  const UserDropdown = () => (
+    <div className="user-dropdown-container">
+      <Dropdown renderToggle={renderIconButton}>
+        <div className="dropdown-menu">
+          <Dropdown.Item className="dropdown-item">Profile</Dropdown.Item>
+          <Dropdown.Item className="dropdown-item">Log Out</Dropdown.Item>
+        </div>
+      </Dropdown>
+    </div>
+  );
+
   return (
     <>
       <header className="header">
         <div className="header-content">
-          <h1 className="title">Timeless Gallery </h1>
+          <h1 className="title">Timeless Gallery</h1>
           {user && <h2 className="title">{user.name}</h2>}
-          <button onClick={toggle} className="login-icon">
-            <IoPerson />
-          </button>
+          {isUserloggedIn ? (
+            <UserDropdown />
+          ) : (
+            <button onClick={toggle} className="login-icon">
+              <IoPerson />
+            </button>
+          )}
         </div>
         <Navbar />
       </header>
-      {isUserloggedIn ? (
-        <div>Hola usuario</div>
-      ) : (
+      {!isUserloggedIn &&
         seen &&
         (isSignUp ? (
           <SignUp toggle={toggle} toggleForm={toggleForm} />
         ) : (
           <LogIn toggle={toggle} toggleForm={toggleForm} />
-        ))
-      )}
+        ))}
     </>
   );
 }
