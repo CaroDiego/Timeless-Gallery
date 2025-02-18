4fd0122844202d6ed718630f7063f1c3c771599b
CaroDiego <dicafre1404@gmail.com>
2025-02-18 13:17:45 +0100

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css"; // Asegúrate de tener estilos para estos componentes.
-import HomeScreen from "./pages/HomeScreen";
+import { lazy, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+
+const HomeScreen = lazy(() => import("./pages/HomeScreen"));
+
 function App() {
   return (
-    <div className="App">
-      <HomeScreen />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+      </Routes>
+    </Suspense>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 import { UserProviderWrapper } from "./context/usercontext.jsx";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
-  <UserProviderWrapper>
-    <App />
-  </UserProviderWrapper>
+  <BrowserRouter>
+    <UserProviderWrapper>
+      <App />
+    </UserProviderWrapper>
+  </BrowserRouter>
   // </StrictMode>,
 );
